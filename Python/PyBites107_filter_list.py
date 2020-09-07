299a3e2f8ba98eb14c27e19a9b70937a28d68574
teddiur <ryaqueiroz@gmail.com>
2020-09-06 22:06:38 -0300

    rearrange repo

--- Python/PyBites107_filter_list.py
+++
@@ -1,6 +0,0 @@
-def filter_positive_even_numbers(numbers):
-    """Receives a list of numbers, and returns a filtered list of only the
-       numbers that are both positive and even (divisible by 2), try to use a
-       list comprehension."""
-    return [number for number in numbers if number > 0 and number%2 == 0]
-
--- Python/PyBites1_sum_n_numbers.py
+++
@@ -1,6 +0,0 @@
-def sum_numbers(numbers = [x for x in range(1,101)]):
-    try:
-        total = sum(numbers)
-    except TypeError:
-        total = sum([x for x in range(1,101)])
-    return total
